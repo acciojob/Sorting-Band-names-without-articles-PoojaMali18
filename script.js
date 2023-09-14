ad5d121f188172eccb03e837c1a592a00b70442c
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-14 12:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let regexp = /\bthe\b|\ban\b|\ba\b/gi;
-//console.log(str.replace(regexp," "));
 
-let arr=['The Virupaksha Temple', 'a Victoria Memorial', 'an Tajmahal'];
-let articleless=[];
-let mp={};
+let arr = ['The Virupaksha Temple', 'a Victoria Memorial', 'an Tajmahal'];
+let articleless = [];
+let mp = {};
 
-for(let i=0;i<arr.length;i++)
-	{
-		let articlelessItem=arr[i].replace(regexp,"").trim();
-		articleless.push(articlelessItem);
-		mp[articlelessItem]=arr[i];
-	}
+for (let i = 0; i < arr.length; i++) {
+  let articlelessItem = arr[i].replace(regexp, "").trim();
+  articleless.push(articlelessItem);
+  mp[articlelessItem] = arr[i];
+}
+
 articleless.sort();
-let ans= [];
-for(let i of articleless)
-	{
-		ans.push(mp[i]);
-	}
+let ans = [];
+for (let i of articleless) {
+  ans.push(mp[i]);
+}
+
 console.log(ans);
